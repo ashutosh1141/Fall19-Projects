668b97fac1759e9dd4216ec9717fefe3cdf2353f
ashutosh1141 <41296283+ashutosh1141@users.noreply.github.com>
2019-06-02 18:53:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -21,3 +21,33 @@ def update_progress(job_title, progress):
 #     update_progress("Some job", i/100.0)
 # update_progress("Some job", 1)
 read_csvfile("./cv_server_data.csv")
+
+
+
+
+# import numpy as np
+# import sesd
+# import matplotlib as ml
+# import matplotlib.pyplot as plt
+
+# ts = np.random.random(100)
+# index=[]
+# for i in range(100):
+# 	index.append(i)
+# # Introduce artificial anomalies
+# ts[14] = 9
+# ts[83] = 10
+
+
+# outliers_indices = sesd.seasonal_esd(ts, hybrid=True, max_anomalies=2)
+# for idx in outliers_indices:
+#     print ("Anomaly index: {0}, anomaly value: {1}".format(idx, ts[idx]))
+# # y_values=[]
+# # for item in outliers_indices:
+# # 	y_values.append(ts[item])
+# # plt.scatter(index, ts)
+
+# # plt.show()
+# plt.plot(index, ts, label='some graph')
+# plt.plot(index,ts,markevery=outliers_indices, ls="", marker="o", label="points")
+# plt.show()
