098c3bfa5b10fd07d567c4560487ce1aa71ccc89
ashutosh1141 <41296283+ashutosh1141@users.noreply.github.com>
2019-06-02 17:26:04 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-x=input('what is your name? ')
-print(x)
-y=input('what is your address? ')
-print(y)
+import time, sys
+import pandas as pd
+
+def read_csvfile(filepath):
+	update_progress("Loading Dataframe Started",0.5)
+	df=pd.read_csv(filepath)
+	update_progress("Loading Dataframe", 1)
+
+
+def update_progress(job_title, progress):
+    length = 20 # modify this to change the length
+    block = int(round(length*progress))
+    msg = "\r{0}: [{1}] {2}%".format(job_title, "#"*block + "-"*(length-block), round(progress*100, 2))
+    if progress >= 1: msg += " DONE\r\n"
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
+# Test
+# for i in range(100):
+#     time.sleep(0.1)
+#     update_progress("Some job", i/100.0)
+# update_progress("Some job", 1)
+read_csvfile("./cv_server_data.csv")
